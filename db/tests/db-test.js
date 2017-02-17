0f9e18619b09de3160e427eb92f9913366555ad2
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-17 15:00:02 -0800

    (refactor) modify test for db

--- db/tests/db-test.js
+++ db/tests/db-test.js
@@ -2,3 +2,19 @@ import { Sequelize } from 'sequelize';
 import { mysql } from 'mysql';
 import { expect } from 'chai'; //check this line of code
 import Students from './studentModel.js';
+
+const db = new Sequelize('computervisiontest', 'root', '');
+
+describe('Students', () => {
+  it('Should have a username and password', () => {
+    let student = connection.define('student', Students);
+    connection.sync().then(() => {
+      student.create({
+        username: 'straightAstudent'
+        password: 'aplus4lyfe'
+      }).then((savedStudent) => {
+        expect(savedStudent.username).to.equal('straightAstudent');
+      });
+    });
+  });
+}
