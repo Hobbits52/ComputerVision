507d0c82cb3c5db23df8776e6e56421f6a4d6495
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-17 12:12:34 -0800

    (feat) add students schema to index.js

---
+++ db/index.js
@@ -0,0 +1,22 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('computervision', 'root', '');
+var bcrypt = require('bcrypt-nodejs');
+
+var Students = db.define('Students', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+});
+
+Students.beforeCreate(function(student, options, cb) {
+  return bcrypt.hash(student.password, null, null, function(err, hashedPw) {
+    if (!err) {
+      student.password = hashedPw;
+      cb(null, options);
+    } else {
+      throw err;
+    }
+  });
+});
+
+
+exports.Students = Students;
