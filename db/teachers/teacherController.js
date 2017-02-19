1a1095c705e0c7e59e5e7b721a8f7e83e7063bec
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-18 18:02:05 -0800

    (feat) add teacher controller

---
+++ db/teachers/teacherController.js
@@ -0,0 +1,42 @@
+const Teachers = require('./teacherModel.js');
+const bcrypt = require('bcrypt-nodejs');
+//TODO: Refactor with promises on server side
+
+exports.teacherSignup = (teacherInput, cb) => {
+  let username = teacherInput.username;
+  let password = teacherInput.password;
+
+  Students.findOrCreate({where: {username: username},
+    defaults: {password: password}})
+  .spread((teacher, created) => {
+    if (created === false) {
+      let error = "Username already in use";
+      cb(error);
+    } else {
+      let newUser = {};
+      newUser.username = teacher.username;
+      cb(null, newUser);
+    }
+  });
+};
+
+exports.teacherLogin = (teacherInput, cb) => {
+  let username = teacherInput.username;
+  let password = teacherInput.password;
+
+  Teachers.findOne({where: {username: username}})
+  .then((teacher) => {
+    bcrypt.compare(password, teacher.password, (err, res) => {
+    if (res !== true) {
+      let error = "Username/Password do not match";
+      cb(error);
+    } else {
+      let newUser = {};
+      newUser.username = teacher.username;
+      cb(null, newUser);
+    }
+    })
+  }).catch((err) => {
+    cb(err);
+  });
+};
