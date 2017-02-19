e85c892a1bc80485eae2ebc3a72e6cc9553c88b8
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-18 17:12:31 -0800

    (feat) Render a react router component, rather than app component.

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes.jsx';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
-}
-
-render(<App/>, document.getElementById('app'));
+render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
