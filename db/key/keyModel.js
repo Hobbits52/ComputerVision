a49ef286bb639326afd834cd3acb86c612f1bf00
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-20 12:26:48 -0800

    (feat) add key schema with min fields for MVP

--- db/key/keyModel.js
+++ db/key/keyModel.js
@@ -1,11 +1,14 @@
 const db = require('./../index.js').db;
 const Sequelize = require('sequelize');
 
+//MVP: 1 key. No associations with courses and teacher
+//MVP+: Multiple teachers. Need foreign key with teacher_id
+//MVP++: Foreign key for course 
+//MVP+++: Add names english names to the keys and english names to the templates (instead of just using key ID to display on front-end)
 const Keys = db.define('Keys', {
-
+  answers: Sequelize.TEXT,
+  URL: Sequelize.TEXT
 }, {timestamps: false});
-
-Keys.belongsTo
 
 Keys.sync();
 
