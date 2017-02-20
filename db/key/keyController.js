d3b6a008ad3f0b9abaf04a96c61bbb270ac0cce7
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-20 14:52:46 -0800

    (test) add require statements for key tests

--- db/key/keyController.js
+++ db/key/keyController.js
@@ -27,3 +27,5 @@ exports.addKey = function(key, cb) {
   });
 };
 
+
+
---
+++ spec/db/dbspec-key.js
@@ -0,0 +1,5 @@
+const Sequelize = require('sequelize');
+const mysql = require('mysql');
+const expect = require('chai').expect;
+const Keys = require('./../../db/student/studentModel.js').Keys;
+
