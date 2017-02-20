cdea943e563fb7fe71155385cef04a3ce2bd4f63
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-20 14:47:38 -0800

    (feat) add getAnswers and addKey controller function

---
+++ db/key/keyController.js
@@ -0,0 +1,29 @@
+const Keys = require('./keyModel.js');
+
+//MVP: 1 key. No input from server controller except callback
+exports.getAnswers = function(cb) {
+  //MVP+ : key will be based on keyInput.id
+  var keyId = 1;
+  Keys.findOne({where: {id: keyId}})
+  .then(function(key) {
+    cb(null, key.answers);
+  }).catch(function(err) {
+    cb(err);
+  })
+};
+
+exports.addKey = function(key, cb) {
+  var URL = key.URL;
+  //either convert array to string here, or it's already a string
+  var answers = key.answers;
+  Keys.create({
+    answers: answers,
+    URL: URL
+  })
+  .then(function(savedKey) {
+    cb(null, savedKey);
+  }).catch(function(err) {
+    cb(err);
+  });
+};
+
