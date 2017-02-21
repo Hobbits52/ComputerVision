6f181b3db4e51605c175f56cf366534a848da2e2
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-20 20:50:53 -0800

    (feat) Add routes for Login and Signup pages

--- src/client/app/routes.jsx
+++ src/client/app/routes.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './App.jsx';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
+
+import TestResults from './TestResults.jsx';
+// Need to add a route for the above!
 
 export default (
   <Route path="/" component={App}>
-    {
-    /*<IndexRoute component={Home} />
-    <Route path="somePath" component={SomeComponent} /> */
-    }
+    <IndexRoute component={Login} />
+    <Route path="signup" component={Signup} />
   </Route>
 )
