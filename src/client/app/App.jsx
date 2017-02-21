b60b9ae668bb7de37ae5b619855e3fef9d3a8670
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-20 20:51:42 -0800

    (feat) Update App.jsx render function.  Return this.props.children.

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {browserHistory} from 'react-router';
+import Login from './Login.jsx';
 import css from '../css/construction.css';
 
 class App extends React.Component {
@@ -33,15 +34,16 @@ class App extends React.Component {
   }
 // --------------------------------------------------------------------
 
+// <div className="construction">
+//   <p>Under Construction!</p>
+// </div>
 
-  render () {
-    return (
-      <div className="construction">
-        <p>Under Construction!</p>
-      </div>
-    );
+  render() {
+    return this.props.children;
   }
 
 }
 
 export default App;
+
+
