f3d0e43d4ecba6c803ea09bcbfdbd1385c1e1da5
Kevin Gin <kevinagin@gmail.com>
2017-02-22 12:38:29 -0800

    (spec) runs test for Scanner

--- spec/server/serverspec.js
+++ spec/server/serverspec.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const request = require('supertest');
 const Teachers = require('./../../db/teacher/teacherModel.js').Teachers;
 const Sequelize = require('sequelize');
+const spawn = require('child_process').spawn;
 // ----------------------------------------------------------------------
 // 'really-need' replaces Node's require with a more powerful version. In 
 // addition to a path, it accepts an options object; one of the available
@@ -112,3 +113,70 @@ xdescribe('Fetching image from Cloudinary', () =>{
     })
   });
 });
+
+describe('Scanner', () =>{
+
+  it('Correctly Grades Test Hosted On Cloudinary', (done) => {
+
+    // run Python as child process
+    const py = spawn('python', ['./src/server/utility/scanner.py']);
+    var testUrl = 'http://res.cloudinary.com/dn4vqx2gu/image/upload/v1487791060/test1resized_v0swpw.jpg'
+   
+    let dataString = '';
+
+    py.stdout.on('data', function(data) {
+      dataString += data.toString();
+    });
+
+    py.stdout.on('end', function() {
+      var answers = JSON.parse(dataString)
+      expect(answers[1][0]).to.equal('a')
+      expect(answers[2][0]).to.equal('c')
+      expect(answers[3][0]).to.equal('b')
+      expect(answers[4][0]).to.equal('d')
+      expect(answers[5][0]).to.equal('e')
+      expect(answers[6][0]).to.equal('c')
+      expect(answers[1].length).to.equal(1)
+      expect(answers[2].length).to.equal(1)
+      expect(answers[3].length).to.equal(1)
+      expect(answers[4].length).to.equal(1)
+      expect(answers[5].length).to.equal(1)
+      expect(answers[6].length).to.equal(1)
+      expect(answers[7].length).to.equal(0)
+      expect(answers[8].length).to.equal(0)
+      expect(answers[9].length).to.equal(0)
+      expect(answers[10].length).to.equal(0)
+      expect(answers[11].length).to.equal(0)
+      expect(answers[12].length).to.equal(0)
+      expect(answers[13].length).to.equal(0)
+      expect(answers[14].length).to.equal(0)
+      expect(answers[15].length).to.equal(0)
+      expect(answers[16].length).to.equal(0)
+      expect(answers[17].length).to.equal(0)
+      expect(answers[18].length).to.equal(0)
+      expect(answers[19].length).to.equal(0)
+      expect(answers[20].length).to.equal(0)
+      expect(answers[21].length).to.equal(0)
+      expect(answers[22].length).to.equal(0)
+      expect(answers[23].length).to.equal(0)
+      expect(answers[24].length).to.equal(0)
+      expect(answers[25].length).to.equal(0)
+      expect(answers[26].length).to.equal(0)
+      expect(answers[27].length).to.equal(0)
+      expect(answers[28].length).to.equal(0)
+      done();
+    });
+
+    py.stdin.write(JSON.stringify(testUrl));
+    py.stdin.end();    
+  });
+});
+
+
+
+
+
+
+
+
+
