c3c6ccfc3599de1d1a668536bc4004e6f0a970b7
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-23 12:02:07 -0800

    (feat) Create KeysView component.  Currently just a placeholder (WIP)

---
+++ src/client/app/KeysView.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {browserHistory} from 'react-router';
+import Nav from './Nav/Nav.jsx'
+import NavSide from './Nav/NavSide.jsx'
+import TeacherViewContainer from './TeacherViewContainer.jsx'
+import HomeView from './HomeView.jsx'
+import Login from './Login.jsx';
+import css from '../css/nav.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // State variables to go here
+    };
+
+    // this.handleSomeEvent = this.handleSomeEvent.bind(this);
+  }
+
+// --------------------------------------------------------------------
+// Component Lifecycle Functions
+// --------------------------------------------------------------------
+
+  componentDidMount() {
+    
+  }
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+// Event Handlers
+// --------------------------------------------------------------------
+
+  handleSomeEvent(someParameter) {
+    //
+  }
+// --------------------------------------------------------------------
+
+// <div className="construction">
+//   <p>Under Construction!</p>
+// </div>
+
+  render() {
+    return (
+      <div>
+        {"KeysView"}
+      </div>
+    );
+  }
+
+}
+
+export default App;
+
+//{this.props.children}
