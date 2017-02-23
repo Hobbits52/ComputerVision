2c68174b7b80bdb9170ba7cb356530b31235a696
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-23 11:40:36 -0800

    (feat) Add routes for TeacherViewContainer and all views rep in sidebar
    Sidebar views include HomeView, StudentsView, ClassesView, KeysView, and StatisticsView

--- src/client/app/routes.jsx
+++ src/client/app/routes.jsx
@@ -4,13 +4,41 @@ import { Route, IndexRoute } from 'react-router';
 import App from './App.jsx';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
+import TeacherViewContainer from './TeacherViewContainer.jsx';
+import HomeView from './HomeView.jsx';
+import StudentsView from './StudentsView.jsx';
+import ClassesView from './ClassesView.jsx';
+import KeysView from './KeysView.jsx';
+import StatisticsView from './StatisticsView.jsx';
 
+import TestResultsContainer from './TestResultsContainer.jsx';
 import TestResults from './TestResults.jsx';
+import TestTakerResults from './TestTakerResults.jsx';
 // Need to add a route for the above!
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={Login} />
-    <Route path="signup" component={Signup} />
+    <IndexRoute component={TeacherViewContainer} />
+      <IndexRoute component={HomeView} />
+      <Route path="/" component={HomeView} />
+      <Route path="students" component={StudentsView} />
+      <Route path="classes" component={ClassesView} />
+      <Route path="keys" component={KeysView} />
+      <Route path="statistics" component={StatisticsView} />
+
   </Route>
 )
+
+// export default (
+//   <Route path="/" component={App}>
+//     <IndexRoute component={Login} />
+//     <Route path="signup" component={Signup} />
+//     <Route path="testresults" component={TestResultsContainer}>
+//       <IndexRoute component={TestResults} />
+//       <Route path=":testTaker" component={TestTakerResults} />
+//     </Route>
+//   </Route>
+// )
+// 
+//       <IndexRoute component={TestResults} />
+      // <Route path=":testTaker" component={TestTakerResults} />
