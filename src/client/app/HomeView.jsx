e43442556f627423ebf9950a1cfb149c5a895278
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-23 23:58:02 -0800

    (feat) add post it notes

--- src/client/app/HomeView.jsx
+++ src/client/app/HomeView.jsx
@@ -38,16 +38,12 @@ class HomeView extends React.Component {
   render() {
     return (
       <div className="col-sm-10 mainContent">
-        <h3>My Classes</h3>
-        <p>
-          {"The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers."}
-        </p>
-        <p>
-        {"And you will know My name is the Lord when I lay My vengeance upon thee.  My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too."} 
-        </p>
-        <p>
-        {"Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?  Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing."}
-        </p>
+        <h3>To-Do List</h3>
+          <div className="postIts">
+            <img className="postImage" src={'../public/post-it.png'}/>
+            <img className="postImage" src={'../public/post-it.png'}/>
+            <img className="postImage" src={'../public/post-it.png'}/>
+          </div>
         <h3>Most Recent Test Results</h3>
         <div className="testTitle">
           <h4>Test Title</h4>
