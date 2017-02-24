cf4bee471e129613a423b834716c59148d412fa2
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-23 20:20:20 -0800

    (feat) begin styling on main dashboard space

--- src/client/app/Nav/NavSide.jsx
+++ src/client/app/Nav/NavSide.jsx
@@ -36,36 +36,6 @@ const NavSide = props => (
         <img className="menuIcons" alt="Statistics" src={'../../../public/pie.png'}/>
         <Link to="statistics">Statistics</Link>
         <img className="hiddenArrow" alt="Home" src={'../../../public/arrow.png'}/>
-        <img className="left-12"
-             alt="User Picture"
-             src={'../../../public/apple.png'}
-             width="150"/>
-      </div>
-      <div className="name">S.L. Jackson, PhD</div>
-      <div className="listItem">
-        <img className="menuIcons" alt="Home" src={'../../../public/white-home.png'}/>
-        <Link to="/">Home</Link>
-        <img className="hiddenArrow" alt="Home" src={'../../../public/arrow.png'}/>
-      </div>
-      <div className="listItem">
-        <img className="menuIcons" alt="Students" src={'../../../public/user.png'}/>
-        <Link to="students">Students</Link>
-        <img className="hiddenArrow" alt="Home" src={'../../../public/arrow.png'}/>
-      </div>
-      <div className="listItem">
-        <img className="menuIcons" alt="Classes" src={'../../../public/white-pencil.png'}/>
-        <Link to="classes">Classes</Link>
-        <img className="hiddenArrow" alt="Home" src={'../../../public/arrow.png'}/>
-      </div>
-      <div className="listItem">
-        <img className="menuIcons" alt="Keys" src={'../../../public/key.png'}/>
-        <Link to="keys">Keys</Link>
-        <img className="hiddenArrow" alt="Home" src={'../../../public/arrow.png'}/>
-      </div>
-      <div className="listItem">
-        <img className="menuIcons" alt="Statistics" src={'../../../public/pie.png'}/>
-        <Link to="statistics">Statistics</Link>
-        <img className="hiddenArrow" alt="Home" src={'../../../public/arrow.png'}/>
       </div>
     </div>
   </div>
