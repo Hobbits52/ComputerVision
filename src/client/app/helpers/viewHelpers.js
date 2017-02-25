5121ef3d77e8501248ef4c1cd9a53d5a1d7ada23
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-25 15:48:18 -0800

    (feat) Begin working on helper functions to initiate axios calls

---
+++ src/client/app/helpers/viewHelpers.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+
+// --------------------------------------------------------------------------
+// HomeView Helpers
+// --------------------------------------------------------------------------
+
+exports.getAllTeachersClasses = (teacherId) => {
+  return axios.get('api/allTeachersClasses', {
+    params: {
+      // I might need to change the property names here?
+      teacher_id: teacherId
+    }
+  }); 
+};
+
+// --------------------------------------------------------------------------
+// Example Route for this axios request:
+// --------------------------------------------------------------------------
+// In a file called dataRoutes.js, provide me the following route:
+// 
+// router.get('/allTeachersClasses', controller.classes.getAll);
+// --------------------------------------------------------------------------
+// 
+// --------------------------------------------------------------------------
+// Example Controller for this route:
+// --------------------------------------------------------------------------
+// In a file called dataControllers.js, provide the following function:
+// 
+// THIS EXAMPLE IS IN KNEX, NOT SEQUELIZE!!!!
+// 
+// ...but same idea!
+// 
+// classes: {   
+//   getAll: function(req, res) {
+//     console.log(req.query);
+//     var teacher_id = req.query.teacher_id;
+//     knex('Teachers')
+//       .select('*')
+//       .where('teacher_id', '=', teacher_id)
+//       .then(function(data){
+//         res.send(data);
+//       })
+//   },
+//   nextFunctionUnderClasses: function(req, res) {},
+//   etc: function(req, res) {}
+// }
+// 
+// 
+// --------------------------------------------------------------------------
+
+exports.getAllStudentsInClass = (classId) => {
+  return axios.get('api/allClassesStudents', {
+    params: {
+      class_Id: classId
+    }
+  }); 
+};
+
+// router.get('/allClassesStudents', controller.students.getAll);
+
+exports.getAllTestsInClass = (classId) => {
+  return axios.get('api/allClassesTests', {
+    params: {
+      class_Id: classId
+    }
+  }); 
+};
+
+// router.get('/allClassesTests', controller.tests.getAll);
+
+exports.getAllStudentsWhoTookTest = (testId) => {
+  return axios.get('api/allStudentsWhoTookTest', {
+    params: {
+      test_Id: testId
+    }
+  }); 
+};
+
+
+// Need one more function for getting most recent test results...
+
+
+// Other Model Requests:
+
+// exports.getTeachersClasses = (teacherId) => {
+//   return axios.get('api/teachersClasses');
+// };
+
+// exports.addSomeKindOfData = (dataToAdd) => {
+//   return axios({
+//     method: 'POST',
+//     url: 'api/emotionalStateData',
+//     data: emotionalStateEntry
+//   });
+// };
