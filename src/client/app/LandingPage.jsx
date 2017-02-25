535372edf5697628b5d64afc2018b39b54204de0
cbathgate <c.bathgate1@gmail.com>
2017-02-24 21:33:38 -0800

    (feat) add images to landing page

--- src/client/app/LandingPage.jsx
+++ src/client/app/LandingPage.jsx
@@ -2,25 +2,43 @@ import React from 'react';
 import {Link} from 'react-router';
 import axios from 'axios';
 import css from '../css/main.css';
+import NavTop from './Nav/NavTop.jsx';
 
 class LandingPage extends React.Component {
   constructor(props) {
     super(props)
-
     this.state = {
 
-    }
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <NavTop />
+        <div className="landingPage">
+          <div className="mainContent">
+            <div>
+              <img src={"./../public/teachers-choice.jpg"} alt="Number 1 Teachers Choice"/>
+            </div>
+            <div>
+              <h1>Grade Less</h1>
+              <h1>Teach More</h1>
+              <input type="submit" value="Login" />
+            </div>
+            <div>
+              <img src={"./../public/goodbye.jpg"} alt="Good-bye Scantrons"/>
+            </div>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-render() {
-  return (
-    <div>
-      <NavTop />
-      <div className="landingPage">
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-  );
-}
+export default LandingPage;
