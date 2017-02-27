2414db310d00675c1be6f881bcad0d3411a07b88
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-26 18:03:38 -0800

    (feat) Create StudentsListEntry component

---
+++ src/client/app/StudentsListEntry.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Link, browserHistory} from 'react-router';
+import css from '../css/nav.css';
+
+class StudentsListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // State variables to go here
+    };
+
+    // this.handleSomeEvent = this.handleSomeEvent.bind(this);
+  }
+
+// --------------------------------------------------------------------
+// Component Lifecycle Functions
+// --------------------------------------------------------------------
+
+  componentDidMount() {
+    
+  }
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+// Event Handlers
+// --------------------------------------------------------------------
+
+  handleSomeEvent(someParameter) {
+    
+  }
+// --------------------------------------------------------------------
+
+  render() {
+    return (
+      <tr onClick={this.props.handleStudentListEntryClick}>
+        <td>{this.props.student.id}</td>
+        <td>{this.props.student.name}</td>
+        <td>{this.props.student.class}</td>
+      </tr>
+    );
+  }
+}
+
+export default StudentsListEntry;
