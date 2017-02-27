537442f8672d8c15dca087df2bc78f85af1c9c80
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-26 23:47:20 -0800

    (feat) Update compWillMount, handleStudentListEntryClick, and render

--- src/client/app/StudentsView.jsx
+++ src/client/app/StudentsView.jsx
@@ -3,6 +3,7 @@ import {render} from 'react-dom';
 import {browserHistory} from 'react-router';
 import {getAllStudents} from './helpers/viewHelpers.js';
 import StudentsList from './StudentsList.jsx';
+import StudentResults from './StudentResults.jsx';
 import css from '../css/nav.css';
 
 class StudentsView extends React.Component {
@@ -15,7 +16,7 @@ class StudentsView extends React.Component {
                  {id: 1, name: 'Anthony Pecchillo', class: 'BIO-101'},
                  {id: 1, name: 'Anthony Pecchillo', class: 'BIO-101'},
                  {id: 1, name: 'Anthony Pecchillo', class: 'BIO-101'}],
-      currenteStudent: null
+      currentStudent: null
     };
 
     this.handleStudentsListEntryClick = this.handleStudentsListEntryClick.bind(this);
@@ -26,11 +27,12 @@ class StudentsView extends React.Component {
 // --------------------------------------------------------------------
 
   componentWillMount() {
-    // getAllStudents().then((res) => {
-    //   this.setState({
-    //     students: res.data
-    //   });
-    // });
+    getAllStudents(1).then((res) => { 
+      console.log('aaa', res);
+      this.setState({
+        students: Array(res.data[0].students[1])
+      });
+    });
   }
 // --------------------------------------------------------------------
 
@@ -40,27 +42,37 @@ class StudentsView extends React.Component {
 // --------------------------------------------------------------------
 
   handleStudentsListEntryClick(event) {
-      console.log('You clicked a StudentListEntry!');
-      console.log('sdfasdf', event.target);
-      this.setState({
-        currentStudent: event.target.id
-      }, () => this.props.router.push('/dashboard'));
-      console.log('this.state.students inside StudentView is: ', this.state.students);
-      console.log('this.state.currentStudent inside StudentView is: ', this.state.currentStudent);
-    // })
+    console.log('You clicked a StudentListEntry!');
+    console.log('sdfasdf', event.target);
+    this.setState({
+      // currentStudent: event.target.id   THIS IS NOT WORKING - FIX IT LATER!
+      currentStudent: true
+    });
+    // }, () => this.props.router.push('studentresults'));   SAME AS ABOVE
+    console.log('this.state.students inside StudentView is: ', this.state.students);
+    console.log('this.state.currentStudent inside StudentView is: ', this.state.currentStudent);
+  // })
   }
 // --------------------------------------------------------------------
 
   render() {
-    return (
-      <div className="innerViews">
-        <h3>Students</h3>
-        <StudentsList students={this.state.students}
-                      currentStudentId={this.state.currentStudent}
-                      handleStudentListEntryClick={this.handleStudentsListEntryClick}
-        />
-      </div>
-    );
+    if (this.state.currentStudent === null) {
+      return (
+        <div>
+          <h2>Students</h2>
+          <StudentsList students={this.state.students}
+                        currentStudentId={this.state.currentStudent}
+                        handleStudentListEntryClick={this.handleStudentsListEntryClick} 
+          />
+        </div>
+      );  
+    } else {
+      return (
+        <div>
+          <StudentResults />
+        </div>
+      );
+    }
   }
 }
 
