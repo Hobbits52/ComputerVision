a15432d5601b815e6192a9c0aae5de4c6aa7463d
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-26 16:02:07 -0800

    (fix) Remove database connection.

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,5 @@
 const app = require('./server-config.js');
 const port = process.env.PORT || 8080;
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'computervision'
-});
-
-connection.connect();
 
 const server = app.listen(port);
 
