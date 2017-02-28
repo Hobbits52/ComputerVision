159e518892cc36ffd7ffd621cd05099103f137a6
Benze Gong <Benze Gong>
2017-02-27 16:30:10 -0800

    added userLogout to module.exports in auth.js

--- src/server/middleware/authentication.js
+++ src/server/middleware/authentication.js
@@ -93,5 +93,6 @@ module.exports = {
   'userLogin' : userLogin,
   'userSignup' : userSignup,
   'studentLogin:' : studentLogin,
-  'studentSignup' : studentSignup
+  'studentSignup' : studentSignup,
+  'userLogout' : userLogout
 };
