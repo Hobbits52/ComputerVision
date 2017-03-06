c71332fe8f41d2253192df92b5ff0a193f900bb2
anthonypecchillo <anthonypecchillo@gmail.com>
2017-03-06 14:54:02 -0800

    (feat) Build out Histogram component

--- src/client/app/data_visualisation/Histogram.jsx
+++ src/client/app/data_visualisation/Histogram.jsx
@@ -1,28 +1,70 @@
 import React from 'react';
 import * as d3 from "d3";
-import DataCircles from './DataCircles.jsx';
-import XYAxis from './XYAxis.jsx';
+import XAxis from './XAxis.jsx';
+import YAxis from './YAxis.jsx';
+import Bar from './Bar.jsx';
 
+class Histogram extends React.Component {
 
-// const marshalProps = (props) => {
-//   const scales = { xScale: xScale(props), yScale: yScale(props) };   // To Marshall Data:  To massage it into a useable form.
-//   return Object.assign({}, props, scales);                           //   Origin:  Ancient Greek Data Scientist, SoSmartOcles 
-// };
-// ----------------------------------------------------------------------------
+  render() {
+    let { top, right, bottom, left, gaussData, width, height, padding } = this.props;
 
-const Histogram = d3.histogram().domain([0,1]).thresholds(19);
-  // const d3Props = marshalProps(props);
-  return ( 
-    <svg width={d3Props.width} height={d3Props.height}>
-    </svg>
-  );
+    console.log('Im Gaussian!: ', gaussData);
+
+    let xScale = 
+      d3.scaleLinear()
+        .domain([0, 100])         // d3.max(gaussData) instead of 100
+        .rangeRound([0, width]);
+
+    let histogramGenerator = (data) => 
+      d3.histogram()
+        .domain(xScale.domain())
+        .thresholds(xScale.ticks(20))
+        (data);
+
+    let histogramData = histogramGenerator(gaussData);
+    console.log('From Histogram, this is histogramData: ', histogramData);
+
+    let yScale = 
+      d3.scaleLinear()
+        .domain([0, d3.max(histogramData, (d) => d.length )])
+        .range([height, 0]);
+
+    return (
+      <div className="react-d3-histogram">
+        <svg width={width + left + right} height={height + top + bottom}>
+          <g transform={"translate(" + left + "," + top + ")"}>
+            <XAxis className="axis" height={height} scale={xScale} />
+            {histogramData.map((bin, index) => 
+              <Bar binData={bin} 
+                   xScale={xScale} 
+                   yScale={yScale} 
+                   height={height} 
+                   key={index}
+              />
+            )}
+          </g>
+        </svg>
+      </div>
+    );
+  }
 }
 
+Histogram.propTypes = {
+  top: React.PropTypes.number,
+  right: React.PropTypes.number,
+  bottom: React.PropTypes.number,
+  left: React.PropTypes.number,
+  width: React.PropTypes.number.isRequired,
+  height: React.PropTypes.number.isRequired,
+  gaussData: React.PropTypes.arrayOf(React.PropTypes.number).isRequired
+};
+
+Histogram.defaultProps = {
+  top: 20,
+  right: 10,
+  bottom: 30,
+  left: 30
+};
+
 export default Histogram;
-
-var histGenerator = d3.histogram()
-  .domain([0,1])    // Set the domain to cover the entire intervall [0;]
-  .thresholds(19);  // number of thresholds; this will create 19+1 bins
-
-const bins = histGenerator(this.props.gaussData);
-console.log(bins);
