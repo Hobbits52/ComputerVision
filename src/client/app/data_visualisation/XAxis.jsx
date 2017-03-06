03210351b45d394aaaa9a485033240f788b997ee
anthonypecchillo <anthonypecchillo@gmail.com>
2017-03-06 14:56:30 -0800

    (feat) Create XAxis component for histogram

---
+++ src/client/app/data_visualisation/XAxis.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import * as d3 from "d3";
+import Tick from "./Tick.jsx";
+import Path from "./Path.jsx";
+
+class XAxis extends React.Component {
+
+  render() {
+    let { height, scale } = this.props;
+
+    let ticks = scale.ticks(20).map(function(tick, index) {
+      console.log(index);
+      return (
+        <Tick value={tick} 
+              scale={scale} 
+              key={index} 
+        />
+      );
+    });
+
+    return (
+      <g className="axis" transform={"translate(0," + height + ")"}>
+        <Path scale={scale} />
+        <g>{ticks}</g>
+      </g>
+    );
+  }
+}
+
+XAxis.propTypes = {
+  height: React.PropTypes.number.isRequired,
+  scale: React.PropTypes.func.isRequired
+};
+
+export default XAxis;
