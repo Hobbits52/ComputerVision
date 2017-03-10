784cd3aca1df410d1d7eb7ac5bac2d5c4c8c901d
anthonypecchillo <anthonypecchillo@gmail.com>
2017-03-10 07:46:34 -0800

    (feat) Update Bar scaling for BarChart

--- src/client/app/data_visualisation/Bar.jsx
+++ src/client/app/data_visualisation/Bar.jsx
@@ -5,24 +5,24 @@ class Bar extends React.Component {
 
   render() {
     let { binData, xScale, yScale, height } = this.props;
-    // console.log('From Bar, binData is: ', binData);
+    console.log('From Bar, freq is: ', binData);
 
-    let scaledX = xScale(binData.x0);
-    let scaledY = yScale(binData.length);
-    let scaledDx = xScale(binData.x1 - binData.x0);
-    // console.log('scaledX is: ', xScale(binData.x0));
+    let scaledX = xScale(binData.name);
+    let scaledY = yScale(binData.value);
+    let scaledDx = xScale.bandwidth();
+    console.log('scaledX is: ', xScale.bandwidth());
 
     return (
       <g className="axis" transform={"translate(" + scaledX + "," + scaledY + ")"}>
         <rect width={scaledDx} height={height - scaledY} />
-        <text dy="0.85em" y="6" x={scaledDx / 2} textAnchor="middle">{binData.y}</text>
+        <text dy="-0.85em" y="6" x={scaledDx / 2} textAnchor="middle">{binData.name}</text>
       </g>
     );
   }
 }
 
 Bar.propTypes = {
-  binData: React.PropTypes.arrayOf(React.PropTypes.number).isRequired,
+  binData: React.PropTypes.object.isRequired,
   xScale: React.PropTypes.func.isRequired,
   yScale: React.PropTypes.func.isRequired,
   height: React.PropTypes.number.isRequired
